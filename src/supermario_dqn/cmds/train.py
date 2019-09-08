98f60a3d29f40a750304b79c27a8d8b284e51b47
Andrea Conti <contiandrea96@gmail.com>
2019-09-09 00:04:21 +0200

    Add main

--- src/supermario_dqn/cmds/train.py
+++ src/supermario_dqn/cmds/train.py
@@ -3,6 +3,11 @@ Handles supermario training
 """
 
 import torch
+from supermario_dqn.environment import MarioEnvironment
+import supermario_dqn.nn as nn
+import supermario_dqn.preprocess as pr
+import argparse
+
 
 if torch.cuda.is_available():
     _device = torch.device('cuda')
@@ -10,19 +15,45 @@ else:
     _device = torch.device('cpu')
     print('[Warning] using CPU for training')
 
-parameters = {
-    'batch_size': 128,
-    'gamma': 0.999,
-    'eps_start': 0.9,
-    'eps_end': 0.05,
-    'target_update': 10,
-    'save_path': 'model.pt',
-    'save_interval': 10,
-    'memory_size': 10000,
-    'num_episodes': 50,
-    'device': _device,
-    'verbose': 1,
-}
 
 def main():
-    print('not yet implemented')
+    """
+    Starts training, handles many parameters for training and also in order to
+    load a .pt state_dict
+    """
+
+    # parse arguments
+    parser = argparse.ArgumentParser(description='Handle training')
+    parser.add_argument('--batch_size', type=int, default=128,
+                        help='size of each batch used for training')
+    parser.add_argument('--gamma', type=float, default=0.999,
+                        help='discount rate used for Q-values learning')
+    parser.add_argument('--eps_start', type=float, default=0.9,
+                        help='start probability to choose a random action')
+    parser.add_argument('--eps_end', type=float, default=0.05,
+                        help='end probability to choose a random action')
+    parser.add_argument('--target_update', type=int, default=10,
+                        help='number of episodes between each target dqn update')
+    parser.add_argument('--save_interval', type=int, default=10,
+                        help='number of episodes between each network checkpoint')
+    parser.add_argument('--save_path', type=str, default='model.pt',
+                        help='where save trained model')
+    parser.add_argument('--memory_size', type=int, default=10000,
+                        help='size of replay memory')
+    parser.add_argument('--num_episodes', type=int, default=200,
+                        help='number of games to be played before end')
+    parser.add_argument('--verbose', type=int, default=1,
+                        help='verbosity of output')
+    parser.add_argument('--load', type=str, default=None,
+                        help='load a saved state_dict')
+
+    args = vars(parser.parse_args())
+    load = args.pop('load')
+    if load is not None:
+        print(f'Loading from {load}')
+    print(f'Saving model in {args["save_path"]}')
+
+    # create environment, DQN and start training
+    env = MarioEnvironment(3, pr.preprocess)
+    model = nn.create([3, 60, 110], env.n_actions, load_state_from=load)
+    nn.train(model, env, device=_device, **args)
