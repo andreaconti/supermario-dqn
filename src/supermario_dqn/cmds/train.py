34d68eeef4a12c6edb0a62996e1ba1b4f8f53cfb
Andrea Conti <contiandrea96@gmail.com>
2019-09-08 19:39:32 +0200

    add params

--- src/supermario_dqn/cmds/train.py
+++ src/supermario_dqn/cmds/train.py
@@ -2,6 +2,27 @@
 Handles supermario training
 """
 
+import torch
+
+if torch.cuda.is_available():
+    _device = torch.device('cuda')
+else:
+    _device = torch.device('cpu')
+    print('[Warning] using CPU for training')
+
+parameters = {
+    'batch_size': 128,
+    'gamma': 0.999,
+    'eps_start': 0.9,
+    'eps_end': 0.05,
+    'target_update': 10,
+    'save_path': 'model.pt',
+    'save_interval': 10,
+    'memory_size': 10000,
+    'num_episodes': 50,
+    'device': _device,
+    'verbose': 1,
+}
 
 def main():
     print('not yet implemented')
