922126ce4b017b4019cab0f7c2ad26c1988722e9
Andrea Conti <contiandrea96@gmail.com>
2019-09-09 20:48:07 +0200

    Add new logs

--- src/supermario_dqn/cmds/train.py
+++ src/supermario_dqn/cmds/train.py
@@ -28,9 +28,9 @@ def main():
                         help='size of each batch used for training')
     parser.add_argument('--fit_interval', type=int, default=16,
                         help='fit every `fit_interval` examples available')
-    parser.add_argument('--gamma', type=float, default=0.999,
+    parser.add_argument('--gamma', type=float, default=0.9,
                         help='discount rate used for Q-values learning')
-    parser.add_argument('--eps_start', type=float, default=0.9,
+    parser.add_argument('--eps_start', type=float, default=0.8,
                         help='start probability to choose a random action')
     parser.add_argument('--eps_end', type=float, default=0.05,
                         help='end probability to choose a random action')
@@ -42,21 +42,23 @@ def main():
                         help='where save trained model')
     parser.add_argument('--memory_size', type=int, default=10000,
                         help='size of replay memory')
-    parser.add_argument('--num_episodes', type=int, default=200,
+    parser.add_argument('--num_episodes', type=int, default=50,
                         help='number of games to be played before end')
     parser.add_argument('--verbose', type=int, default=1,
                         help='verbosity of output')
     parser.add_argument('--load', type=str, default=None,
                         help='load a saved state_dict')
-    parser.add_argument('--log_file_path', type=str, default='log.txt',
+    parser.add_argument('--log_file_dir', type=str, default=None,
                         help='file path where write logs')
 
     args = vars(parser.parse_args())
 
+    # log params
     print('training parameters:')
-    with open('paramters_log.txt', 'w') as params_log:
-        for k, v in args.items():
-            print('{:15} {}'.format(k, v))
+    for k, v in args.items():
+        print('{:15} {}'.format(k, v))
+    if args['log_file_dir'] is not None:
+        with open('parameters.log', 'w') as params_log:
             params_log.write('{:15} {}\n'.format(k, v))
 
     # create environment, DQN and start training
--- src/supermario_dqn/nn.py
+++ src/supermario_dqn/nn.py
@@ -110,9 +110,9 @@ def create(size: typing.List[int], outputs: int,
 
 
 def train(policy_net: DQN, env: MarioEnvironment, batch_size=128, fit_interval=32,
-          gamma=0.999, eps_start=0.9, eps_end=0.05, eps_decay=200, target_update=10,
+          gamma=0.9, eps_start=0.9, eps_end=0.05, eps_decay=200, target_update=10,
           save_path='model.pt', save_interval=10, memory_size=10000, num_episodes=50,
-          device='cpu', log_file_path=None, verbose=1):
+          device='cpu', log_file_dir=None, verbose=1):
     """
     Handles training of network
     """
@@ -131,30 +131,39 @@ def train(policy_net: DQN, env: MarioEnvironment, batch_size=128, fit_interval=3
     memory = _ReplayMemory(memory_size)
     optimizer = optim.RMSprop(policy_net.parameters())
 
-    if log_file_path is not None:
-        log_file = open(log_file_path, 'w')
+    if log_file_dir is not None:
+        episode_log_file = open(log_file_dir + '/episodes.csv', 'w')
+        episode_log_file.write('episode,reward,steps,choosen_moves,random_moves\n')
+        fitting_log_file = open(log_file_dir + '/fitting_log.csv\n', 'w')
+        fitting_log_file.write('episode,step,mean_error\n')
+        qvalues_log_file = open(log_file_dir + '/qvalues_log.csv', 'w')
+        qvalues_log_file.write('episode,step,' + ','.join(["{}{}".format(a, b) for a, b in zip(['action'] * n_actions, range(n_actions))]) + ',choosen,reward') # noqa
 
-    def log(msg):
-        if log_file_path is not None:
-            log_file.write(msg + '\n')
-        print(msg)
+    # for logs
+    curr_episode = 0  # current episode
+    steps_done = 0  # step in a single episode
+    choosen_moves = 0  # number of choosen moves in a single episode
+    random_moves = 0  # number of random moves in a single episode
 
     # select random action
-    steps_done = 0
+    total_step = 0
 
     def select_action(state):
-        nonlocal steps_done
+        nonlocal total_step, choosen_moves, random_moves
         sample = random.random()
-        eps_threshold = eps_end + (eps_start - eps_end) * math.exp(-1. * steps_done / eps_decay)
-        steps_done += 1
+        eps_threshold = eps_end + (eps_start - eps_end) * math.exp(-1. * total_step / eps_decay)
+        total_step += 1
         if sample > eps_threshold:
+            choosen_moves += 1
             with torch.no_grad():
-                return policy_net(state).max(1)[1].view(1, 1)
+                qvalues = policy_net(state)
+                return qvalues.max(1)[1].view(1, 1), qvalues
         else:
-            return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
+            random_moves += 1
+            return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long), None
 
     # perform a single optimization step
-    def optimize_model(verbose=1):
+    def optimize_model():
 
         if len(memory) < batch_size:
             return
@@ -178,23 +187,28 @@ def train(policy_net: DQN, env: MarioEnvironment, batch_size=128, fit_interval=3
 
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
 
-        if verbose > 1:
-            log(f"Fitting, loss: {loss.mean()}")
-
         optimizer.zero_grad()
         loss.backward()
         for param in policy_net.parameters():
             param.grad.data.clamp_(-15, 15)  # to avoid exploding gradient problem
         optimizer.step()
 
+        if log_file_dir is not None:
+            fitting_log_file.write('{},{},{}\n'.format(curr_episode, total_step, loss.mean()))
+
     # training loop
     try:
         for i_episode in range(num_episodes):
+            curr_episode = i_episode + 1
+            steps_done = 0
+            choosen_moves = 0
+            random_moves = 0
             episode_reward = 0
             curr_state = env.reset()
             done = False
             while not done:
-                action = select_action(curr_state.unsqueeze(0).to(device))
+                steps_done += 1
+                action, qvalues = select_action(curr_state.unsqueeze(0).to(device))
                 next_state, reward, done, _ = env.step(action.item())
                 episode_reward += reward
                 reward = torch.tensor([reward], device=device, dtype=torch.float32)
@@ -209,26 +223,40 @@ def train(policy_net: DQN, env: MarioEnvironment, batch_size=128, fit_interval=3
                 if steps_done % fit_interval == 0:
                     optimize_model()
 
+                # log on qvalues_file_log.csv
+                if log_file_dir is not None and qvalues is not None:
+                    qvalues_log_file.write('{},{},'.format(curr_episode, total_step)
+                                           + ','.join(list(map(lambda t: str(t.item()), qvalues[0])))
+                                           + ',{},{}\n'.format(action.item(), reward.item()))
+
+            # log on episode.csv
+            if log_file_dir is not None:
+                episode_log_file.write('{},{},{},{},{}\n'
+                                       .format(i_episode+1, episode_reward, steps_done, choosen_moves, random_moves))
+
+            # print
             if verbose > 0:
-                log(f'[{datetime.datetime.now().strftime("%d:%m:%Y %H:%M")}] end episode ({i_episode+1}/{num_episodes}): {episode_reward} reward')  # noqa
+                print(f'[{datetime.datetime.now().strftime("%d:%m:%Y %H:%M")}] end episode ({i_episode+1}/{num_episodes}): {episode_reward} reward')  # noqa
 
             # Update the target network, copying all weights and biases in DQN
             if i_episode % target_update == 0:
                 if verbose > 0:
-                    log(f'[{datetime.datetime.now().strftime("%d:%m:%Y %H:%M")}] updating target network')
+                    print(f'[{datetime.datetime.now().strftime("%d:%m:%Y %H:%M")}] updating target network')
                 target_net.load_state_dict(policy_net.state_dict())
 
             # Save on file
             if i_episode % save_interval == 0:
                 if verbose > 0:
-                    log(f'[{datetime.datetime.now().strftime("%d:%m:%Y %H:%M")}] saving model ({steps_done} steps done)')  # noqa
+                    print(f'[{datetime.datetime.now().strftime("%d:%m:%Y %H:%M")}] saving model ({total_step} steps done)')  # noqa
                 torch.save(policy_net.state_dict(), save_path)
 
     finally:
 
         # close logs
-        if log_file_path is not None:
-            log_file.close()
+        if log_file_dir is not None:
+            qvalues_log_file.close()
+            fitting_log_file.close()
+            episode_log_file.close()
 
         # save network
         torch.save(policy_net.state_dict(), save_path)
